c35b8169192e3285683a02b5970271c333f63e13
corner <corner@sgrsoft.com>
2022-08-29 15:29:21 +0900

    player 의존성 추가

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/sgrsoft/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/sgrsoft/IdeaProjects/flutter_test_app"
+export "FLUTTER_APPLICATION_PATH=/Users/sgrsoft/workzone/flutter/flutter_test"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/sgrsoft/IdeaProjects/flutter_test_app/lib/main.dart"
+export "FLUTTER_TARGET=/Users/sgrsoft/workzone/flutter/flutter_test/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVF
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/sgrsoft/IdeaProjects/flutter_test_app/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/sgrsoft/workzone/flutter/flutter_test/.dart_tool/package_config.json"
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
