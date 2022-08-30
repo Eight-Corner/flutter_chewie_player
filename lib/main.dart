5ad76740a3afc9a91b0fc59f42f716bc427553cd
corner <corner@sgrsoft.com>
2022-08-30 15:29:19 +0900

    video _ player

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,8 @@
+import 'package:chewie_video_demo_corner/app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_test_app/component/chewie_list_item.dart';
-import 'package:video_player/video_player.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'video player chewie package Demo',
-      theme: ThemeData.light().copyWith(
-        platform: TargetPlatform.iOS,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('video player chewie package Demo'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          ChewieListItem(
-            videoPlayerController: VideoPlayerController.asset(
-              'instagram_video.mp4',
-            ),
-            looping: true,
-          ),
-          ChewieListItem(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://player.live24.app/vod/2420521/10364'),
-              looping: true)
-        ],
-      ),
-    );
-  }
+  runApp(
+    const ChewieDemo(),
+  );
 }
