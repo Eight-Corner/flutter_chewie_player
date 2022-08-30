556d010d270c7af4d490753dd0dadaf970d3c998
corner <corner@sgrsoft.com>
2022-08-30 14:06:02 +0900

    chewie package

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -12,10 +12,17 @@
 @import video_player_avfoundation;
 #endif
 
+#if __has_include(<wakelock/WakelockPlugin.h>)
+#import <wakelock/WakelockPlugin.h>
+#else
+@import wakelock;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [FLTVideoPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTVideoPlayerPlugin"]];
+  [WakelockPlugin registerWithRegistrar:[registry registrarForPlugin:@"WakelockPlugin"]];
 }
 
 @end
--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/component/chewie_list_item.dart';
 import 'package:video_player/video_player.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'video player chewie package Demo',
+      theme: ThemeData.light().copyWith(
+        platform: TargetPlatform.iOS,
       ),
-      home: const _VideoPlayerScreen(),
+      home: MyHomePage(),
     );
   }
 }
 
-class _VideoPlayerScreen extends StatefulWidget {
-  const _VideoPlayerScreen({Key? key}) : super(key: key);
-
-  @override
-  State<_VideoPlayerScreen> createState() => __VideoPlayerScreenState();
-}
-
-class __VideoPlayerScreenState extends State<_VideoPlayerScreen> {
-  late VideoPlayerController _controller;
-  late Future<void> _initializeVideoPlayerFuture;
-
-
-
-  @override
-  void initState() {
-
-    _controller = VideoPlayerController.network(
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
-    );
-    _initializeVideoPlayerFuture = _controller.initialize();
-
-    super.initState();
-
-  }
-
-  @override
-  void dispose() {
-
-    _controller.dispose();
-    super.dispose();
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Butterfly Video'),
+        title: Text('video player chewie package Demo'),
       ),
-      // VideoPlayerController가 초기화를 진행하는 동안 로딩 스피너를 보여주기 위해
-      // FutureBuilder를 사용합니다.
-      body: FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // 만약 VideoPlayerController 초기화가 끝나면, 제공된 데이터를 사용하여
-            // VideoPlayer의 종횡비를 제한하세요.
-            return AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              // 영상을 보여주기 위해 VideoPlayer 위젯을 사용합니다.
-              child: VideoPlayer(_controller),
-            );
-          } else {
-            // 만약 VideoPlayerController가 여전히 초기화 중이라면,
-            // 로딩 스피너를 보여줍니다.
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+      body: ListView(
+        children: <Widget>[
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'instagram_video.mp4',
+            ),
+            looping: true,
+          ),
+          ChewieListItem(
+              videoPlayerController: VideoPlayerController.network(
+                  'https://player.live24.app/vod/2420521/10364'),
+              looping: true)
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // 재생/일시 중지 기능을 `setState` 호출로 감쌉니다. 이렇게 함으로써 올바른 아이콘이
-          // 보여집니다.
-          setState(() {
-            // 영상이 재생 중이라면, 일시 중지 시킵니다.
-            if (_controller.value.isPlaying) {
-              _controller.pause();
-            } else {
-              // 만약 영상이 일시 중지 상태였다면, 재생합니다.
-              _controller.play();
-            }
-          });
-        },
-        // 플레이어의 상태에 따라 올바른 아이콘을 보여줍니다.
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-        ),
-      ), // 이 마지막 콤마는 build 메서드에 자동 서식이 잘 적용될 수 있도록 도와줍니다.
     );
   }
 }
